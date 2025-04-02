a538c580f48f0edeea53b60505c6a90157b4cafc
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-02 11:59:57 +0100

    Added getSingleAPOD GET method

--- src/app/services/apod.service.ts
+++ src/app/services/apod.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Apod } from '../models/apod';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApodService {
 
-  constructor() { }
+  private _apiURL = environment.APOD_URL;
+  private _apiKey = environment.API_KEY;
+
+  constructor(private _http: HttpClient) { }
+
+  getSingeAPOD(date: string): Observable<Apod> {
+    return this._http.get<Apod>(`${this._apiURL}/?api_key=${environment.API_KEY}&${date}`).pipe(
+      map(response => ({
+          date: response.date,
+          explanation: response.explanation,
+          hdurl: response.hdurl,
+          media_type: response.media_type,
+          service_version: response.service_version,
+          title: response.title,
+          url: response.url
+        }))
+    );
+  }
 }
