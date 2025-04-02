5013275d70dc540e6d5f19f4aa82a75a1ef8bb1c
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-02 21:56:24 +0100

    Added asteroidDetailsURL & getAsteroidDetails method

--- src/app/services/asteroids.service.ts
+++ src/app/services/asteroids.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+import { Asteroid, Asteroids } from '../models/asteroids';
+import { AsteroidDetails } from '../models/asteroidDetails';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsteroidsService {
+  private _apiFeedURL = environment.ASTEROIDS_URL;
+  private _apiNeoURL = environment.ASTEROIDS_DETAILS_URL;
+  private _apiKey = environment.API_KEY;
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getAsteroids(start_date: string, end_date: string): Observable<Asteroids> {
+    return this._http.get<Asteroids>(`${this._apiFeedURL}?api_key=${this._apiKey}&start_date=${start_date}&end_date=${end_date}`);
+  }
+  getAsteroidDetails(asteroidId: string) :Observable<AsteroidDetails> {
+    return this._http.get<AsteroidDetails>(`${this._apiNeoURL}${asteroidId}?api_key=${this._apiKey}`);
+  }
 }
