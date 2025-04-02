59a91b91c06ac0efee43c2eba17cc88f881aa0c0
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-02 19:32:13 +0100

    Added properties & Added getPicture and getPictures methods

--- src/app/components/apod/apod.component.ts
+++ src/app/components/apod/apod.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { CommonModule} from '@angular/common';
+
 import { RouterModule } from '@angular/router';
+import { ApodService } from '../../services/apod.service';
+import { Apod } from '../../models/apod';
 
 @Component({
   selector: 'app-apod',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './apod.component.html',
   styleUrl: './apod.component.css'
 })
 export class ApodComponent {
+  picture?: Apod
+  pictures?: Apod[]
+  imageWidth: string = "600px";
 
+  constructor (private _apodService: ApodService) {}
+
+  getPicture() {
+    this._apodService.getSingleAPOD("2015-12-10").subscribe(
+      picture => {
+        this.picture = picture;
+        console.log(JSON.stringify(picture));
+      }
+    )
+  }
+  getPictures() {
+    this._apodService.getMultipleAPOD("2015-12-10", "2015-12-11").subscribe(
+      pictures => {
+        this.pictures = pictures;
+        console.log(JSON.stringify(pictures));
+      }
+    )
+  }
 }
