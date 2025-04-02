fe051358ec1a6c0b1b7cba1118e079320d63b24a
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-02 10:37:08 +0100

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ApodComponent } from './components/apod/apod.component';
+import { AsteroidsComponent } from './components/asteroids/asteroids.component';
+import { RoversComponent } from './components/rovers/rovers.component';
+import { EpicComponent } from './components/epic/epic.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {path:"", component: AppComponent},
+ {path:"/apod", component: ApodComponent},
+ {path:"/asteroids", component: AsteroidsComponent},
+ {path:"/rovers", component: RoversComponent},
+ {path:"/epic", component: EpicComponent}
+
+];
