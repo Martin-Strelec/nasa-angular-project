b081459dc61c4ca1587cf073267f1a70023915a4
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-02 12:01:22 +0100

    Renamed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,12 @@ import { ApodComponent } from './components/apod/apod.component';
 import { AsteroidsComponent } from './components/asteroids/asteroids.component';
 import { RoversComponent } from './components/rovers/rovers.component';
 import { EpicComponent } from './components/epic/epic.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
- {path:"", component: AppComponent},
- {path:"/apod", component: ApodComponent},
- {path:"/asteroids", component: AsteroidsComponent},
- {path:"/rovers", component: RoversComponent},
- {path:"/epic", component: EpicComponent}
+ {path: "", component: ApodComponent},
+ {path: "apod", component: ApodComponent},
+ {path: "asteroids", component: AsteroidsComponent},
+ {path: "rovers", component: RoversComponent},
+ {path: "epic", component: EpicComponent}
 
 ];
