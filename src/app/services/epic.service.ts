00a37199cee426963dcb61c8676da8d0715236a5
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-07 14:35:45 +0100

    Added getEPICMetadata method

--- src/app/services/epic.service.ts
+++ src/app/services/epic.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EPIC } from '../models/epic';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EpicService {
+  private _apiEPICMetadata = environment.EPIC_URL;
+  private _apiEPICArchive = environment.EPIC_ARCHIVE_URL;
+  private _apiKey = environment.API_KEY;
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getEPICMetadata(date: string, imageType: string): Observable<EPIC[]> {
+    return this._http.get<EPIC[]>(`${this._apiEPICMetadata}${imageType}?api_key=${this._apiKey}`);
+  }
 }
+
