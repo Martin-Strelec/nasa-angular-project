7e1d5292ef73e7c1dc4e437306bee81abf581e3e
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-07 14:34:57 +0100

    Added epic interface

---
+++ src/app/models/epic.ts
@@ -0,0 +1,44 @@
+export interface EPIC {
+  identifier: string
+  caption: string
+  image: string
+  version: string
+  centroid_coordinates: CentroidCoordinates
+  dscovr_j2000_position: DscovrJ2000Position
+  lunar_j2000_position: LunarJ2000Position
+  sun_j2000_position: SunJ2000Position
+  attitude_quaternions: AttitudeQuaternions
+  date: string
+  coords?: string
+  imageUrl: string
+}
+
+export interface CentroidCoordinates {
+  lat: number
+  lon: number
+}
+
+export interface DscovrJ2000Position {
+  x: number
+  y: number
+  z: number
+}
+
+export interface LunarJ2000Position {
+  x: number
+  y: number
+  z: number
+}
+
+export interface SunJ2000Position {
+  x: number
+  y: number
+  z: number
+}
+
+export interface AttitudeQuaternions {
+  q0: number
+  q1: number
+  q2: number
+  q3: number
+}
