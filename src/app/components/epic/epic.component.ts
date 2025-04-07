eec98f18a76db3bd3e4962f86a2641873d79ec08
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-07 14:34:48 +0100

    Imported required modules & Added getEPICs and getPictures method

--- src/app/components/epic/epic.component.ts
+++ src/app/components/epic/epic.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { EPIC } from '../../models/epic';
+import { EpicService } from '../../services/epic.service';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-epic',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './epic.component.html',
   styleUrl: './epic.component.css'
 })
 export class EpicComponent {
+  EPICs: EPIC[] = [];
+  EpicImages: string[] = [];
 
+  constructor(private _epicService: EpicService) { }
+
+  ngOnInit() {
+    this.getEPICs();  
+  }
+  getPictures(epics: EPIC[], imageType: string){
+    epics.forEach(epic => {
+      console.log(`https://api.nasa.gov/EPIC/archive/${imageType}/${epic.date.split(' ')[0].replace(/-/g, '/')}/png/${epic.image}.png?api_key=${environment.API_KEY}`);
+      this.EpicImages.push(`https://api.nasa.gov/EPIC/archive/${imageType}/${epic.date.split(' ')[0].replace(/-/g, '/')}/png/${epic.image}.png?api_key=${environment.API_KEY}`);
+      console.log(this.EpicImages);
+    });
+
+  }
+  getEPICs() {
+    this._epicService.getEPICMetadata('2023-10-01', 'enhanced').subscribe((response: EPIC[]) => {
+      this.EPICs = response;
+      console.log(this.EPICs);
+      this.getPictures(this.EPICs, 'enhanced');
+    });
+  }
 }
