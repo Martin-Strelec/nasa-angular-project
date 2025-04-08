504db5e6e4bf9c0cf189479b1748d3f059717abd
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-08 17:23:05 +0100

    Enhance APOD component with date inputs and loading logic for single and multiple images

--- src/app/components/apod/apod.component.ts
+++ src/app/components/apod/apod.component.ts
@@ -3,30 +3,59 @@ import { CommonModule } from '@angular/common';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 import { ApodService } from '../../services/apod.service';
 import { Apod } from '../../models/apod';
 
 @Component({
   selector: 'app-apod',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './apod.component.html',
   styleUrl: './apod.component.css'
 })
 export class ApodComponent {
-  picture?: Apod
-  pictures?: Apod[]
+  pictures: Apod[] = []
   errorMessage: string = '';
   imageWidth: string = "600px";
+  minVideoWidth: string = "300px";
+  isChecked: boolean = false;
+  startDate: string = '';
+  endDate: string = '';
+  sanitizedURLs: { [key: string]: SafeResourceUrl } = {};
 
-  constructor(private _apodService: ApodService) { }
+  constructor(private _apodService: ApodService, private sanitizer: DomSanitizer) { }
 
   reloadWindow() {
     window.location.reload();
   }
 
-  getPicture() {
-    this._apodService.getSingleAPOD("2015-12-10")
+  ngOnInit(): void {
+    const today = new Date();
+    this.endDate = today.toISOString().split('T')[0]; // Format as YYYY-MM-DD
+    this.startDate = today.toISOString().split('T')[0]; // Format as YYYY-MM-DD
+  }
+
+  onSwitchChange(event: Event) {
+    this.isChecked = (event.target as HTMLInputElement).checked;
+    this.pictures = []; // Clear the pictures array when the switch is toggled
+    console.log('Switch is ' + (this.isChecked ? 'ON' : 'OFF'));
+  }
+
+  loadData() {
+    if (this.isChecked) {
+      console.log(this.startDate, this.endDate);
+      this.getPictures(this.startDate, this.endDate);
+    }
+    else {
+      console.log(this.startDate);
+      this.getPicture(this.startDate);
+    }
+  }
+
+  getPicture(date: string) {
+    this._apodService.getSingleAPOD(date)
       .pipe(
         catchError(error => {
           this.errorMessage = 'Failed to fetch APOD image. Please try again later.';
@@ -35,13 +64,14 @@ export class ApodComponent {
         })
       ).subscribe(
         picture => {
-          this.picture = picture;
+          this.pictures?.push(picture);
+          this.sanitizeURLs(); // Sanitize URLs after fetching the pictures
           console.log(JSON.stringify(picture));
         }
       )
   }
-  getPictures() {
-    this._apodService.getMultipleAPOD("2015-12-10", "2015-12-11")
+  getPictures(startDate: string, endDate: string) {
+    this._apodService.getMultipleAPOD(startDate, endDate)
       .pipe(
         catchError(error => {
           this.errorMessage = 'Failed to fetch APOD Images. Please try again later.';
@@ -52,8 +82,17 @@ export class ApodComponent {
       .subscribe(
         pictures => {
           this.pictures = pictures;
+          this.sanitizeURLs(); // Sanitize URLs after fetching the pictures
           console.log(JSON.stringify(pictures));
         }
       )
+  }
+
+  sanitizeURLs() {
+    this.pictures.forEach(picture => {
+      if (picture.media_type === 'video') {
+        this.sanitizedURLs[picture.url] = this.sanitizer.bypassSecurityTrustResourceUrl(picture.url);
+      }
+    });
   }
 }
