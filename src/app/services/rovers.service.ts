452f3f528487acc61f694d70eca0cb9fc174f6fe
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-10 16:11:32 +0100

    Refactor RoversService methods to remove camera parameter and update return types

--- src/app/services/rovers.service.ts
+++ src/app/services/rovers.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment.development';
-import { Rover, RoverPhoto, Root} from '../models/rover';
+import { RoverPhoto, PhotoRoot, RoverRoot} from '../models/rover';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +15,16 @@ export class RoversService {
 
   constructor(private _http: HttpClient) { }
 
-  getImagesBySol(roverName: string, sol: number, camera:string): Observable<Root> {
-    return this._http.get<Root>(`${this._apiRoverURL}rovers/${roverName}/photos?sol=${sol}&camera=${camera}&api_key=${this._apiKey}`);
+  getImagesBySol(roverName: string, sol: number): Observable<PhotoRoot> {
+    return this._http.get<PhotoRoot>(`${this._apiRoverURL}rovers/${roverName}/photos?sol=${sol}&api_key=${this._apiKey}`);
   }
 
-  getImagesByDate(roverName: string, date: string, camera:string): Observable<Root> {
-    return this._http.get<Root>(`${this._apiRoverURL}rovers/${roverName}/photos?earth_date=${date}&camera=${camera}&api_key=${this._apiKey}`);
+  getImagesByDate(roverName: string, date: string): Observable<PhotoRoot> {
+    console.log(`${this._apiRoverURL}rovers/${roverName}/photos?earth_date=${date}&api_key=${this._apiKey}`);
+    return this._http.get<PhotoRoot>(`${this._apiRoverURL}rovers/${roverName}/photos?earth_date=${date}&api_key=${this._apiKey}`);
   }
 
-  getMissionManifest(roverName: string): Observable<Rover[]> {
-    return this._http.get<Rover[]>(`${this._apiRoverURL}manifests/${roverName}?api_key=${this._apiKey}`);
+  getMissionManifest(roverName: string): Observable<RoverRoot> {
+    return this._http.get<RoverRoot>(`${this._apiRoverURL}manifests/${roverName}?api_key=${this._apiKey}`);
   }
 }
