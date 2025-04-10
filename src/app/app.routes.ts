7320aca4e779d103f9599883885a1a8e4663ec3a
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-10 12:10:49 +0100

    Add route for EpicDetails component and ensure proper formatting in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RoversComponent } from './components/rovers/rovers.component';
 import { EpicComponent } from './components/epic/epic.component';
 import { AsteroidDetailsComponent } from './components/asteroid-details/asteroid-details.component';
 import { ApodDetailsComponent } from './components/apod-details/apod-details.component';
+import { EpicDetailsComponent } from './components/epic-details/epic-details.component';
 
 export const routes: Routes = [
  {path: "", component: ApodComponent},
@@ -13,5 +14,6 @@ export const routes: Routes = [
  {path: "rovers", component: RoversComponent},
  {path: "epic", component: EpicComponent},
  {path: "asteroid-details/:id", component: AsteroidDetailsComponent},
- {path: "apod-details/:date", component: ApodDetailsComponent}
+ {path: "apod-details/:date", component: ApodDetailsComponent},
+ {path: "epic-details/:identifier", component: EpicDetailsComponent}
 ];
