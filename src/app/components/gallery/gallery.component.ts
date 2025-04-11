cad100786b2325fc53370c115f64c9602e45b4df
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-11 10:52:15 +0100

    Refactor GalleryComponent: Update HTML structure and add CommonModule import

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+
+  images: string[] = [];
 
 }
