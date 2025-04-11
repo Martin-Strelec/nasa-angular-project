242853f5683e652dff328bf1c6b3b1acb4b285db
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-11 10:52:08 +0100

    Add GalleryComponent route to app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { EpicComponent } from './components/epic/epic.component';
 import { AsteroidDetailsComponent } from './components/asteroid-details/asteroid-details.component';
 import { ApodDetailsComponent } from './components/apod-details/apod-details.component';
 import { HomeComponent } from './components/home/home.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 export const routes: Routes = [
  {path: "", component: HomeComponent},
@@ -14,5 +15,6 @@ export const routes: Routes = [
  {path: "rovers", component: RoversComponent},
  {path: "epic", component: EpicComponent},
  {path: "asteroid-details/:id", component: AsteroidDetailsComponent},
- {path: "apod-details/:date", component: ApodDetailsComponent}
+ {path: "apod-details/:date", component: ApodDetailsComponent},
+ {path: "gallery", component: GalleryComponent}
 ];
