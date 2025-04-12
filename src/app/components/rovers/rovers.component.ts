cdbf2a13ab2071e19fd41854ebeb6f37edaaf4e2
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-12 22:30:59 +0100

    Add image modal and save functionality to rover component

--- src/app/components/rovers/rovers.component.ts
+++ src/app/components/rovers/rovers.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -8,7 +8,10 @@ import { RoversService } from '../../services/rovers.service';
 import { PhotoManifest, RoverPhoto, PhotoRoot, RoverRoot } from '../../models/rover';
 import { FormsModule } from '@angular/forms';
 import { RoverDetailsComponent } from "../rover-details/rover-details.component";
+import { GalleryService } from '../../services/gallery.service';
+import { NewGalleryImage } from '../../models/galleryImage';
 
+declare var bootstrap: any; // declare bootstrap for TS
 
 @Component({
   selector: 'app-rovers',
@@ -26,16 +29,24 @@ export class RoversComponent {
   photos: RoverPhoto[] = [];
   groupedPhotos: { [key: string]: RoverPhoto[] } = {};
   currentManifest: PhotoManifest | null = null;
+  selectedPhoto?: RoverPhoto;
+  toastType: string = '';
+  toastMessage: string = '';
+  toastInstance: any;
 
-  constructor(private _roversService: RoversService) { }
-  
+  constructor(private _roversService: RoversService, private _gallery: GalleryService) { }
+
   ngOnInit(): void {
     const today = new Date();
     today.setDate(today.getDate() - 2); // Set to two days back
     this.date = today.toISOString().split('T')[0]; // Format as YYYY-MM-DD
-    //this.getRoverManifest(this.roverName); // Initial fetch
+    
+    const toastEl = document.getElementById('liveToast');
+    if (toastEl) {
+      this.toastInstance = new bootstrap.Toast(toastEl);
+    }
   }
-  
+
   reloadWindow() {
     window.location.reload();
   }
@@ -47,7 +58,7 @@ export class RoversComponent {
     console.log('Switch is ' + (this.isChecked ? 'ON' : 'OFF'));
   }
 
-  groupPhotosByCamera(photos: RoverPhoto[]): {[key: string]: any[] } {
+  groupPhotosByCamera(photos: RoverPhoto[]): { [key: string]: any[] } {
     const grouped: { [cameraName: string]: RoverPhoto[] } = {};
 
     photos.forEach(photo => {
@@ -78,7 +89,7 @@ export class RoversComponent {
     // You could set a flag or update UI here
     photo.loaded = true;
   }
-  
+
   onImageError(photo: any) {
     console.warn('Failed to load image:', photo.img_src);
     photo.error = true;
@@ -92,75 +103,108 @@ export class RoversComponent {
     this.currentManifest = null;
   }
 
+  //function to save the image
+  saveImage() {
+    let newImage: NewGalleryImage;
+    if (this.selectedPhoto) {
+      newImage = new NewGalleryImage(this.selectedPhoto.img_src, this.selectedPhoto.earth_date);
+      this._gallery.addImage(newImage).subscribe((response) => {
+        this.toastMessage = 'Image Saved Succesfully!'
+        this.toastType = 'success'
+        this.showToast();
+        console.log('Image saved successfully:', response);
+      }, (error) => {
+        this.toastMessage = 'Error when saving image'
+        this.toastType = 'danger'
+        this.showToast();
+        console.error('Error saving image:', error);
+      });
+    }
+  }
+
+  openModal(photo: RoverPhoto) {
+    this.selectedPhoto = photo;
+    const modalElement = document.getElementById('imageModal');
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
+
+  showToast(type: string = 'success') {
+    this.toastType = type;
+    this.toastInstance?.show();
+  }
+
   getRoverPhotosByDate(roverName: string, date: string): void {
-      this._roversService.getImagesByDate(roverName, date)
-        .pipe(
-          catchError(error => {
-            this.errorMessage = 'Failed to fetch Rover images. Please try again later.';
-            console.error('Error fetching Rover metadata:', error);
-            return throwError(() => error); // Updated to use the new signature
-          })
-        )
-        .subscribe({
-          next: (response: PhotoRoot) => {
-            console.log('Rover metadata:', response);
-            if (response.photos.length === 0) {
-              this.picturesErrorMessage = 'No images found for the selected date.';
-              return;
-            }
-            this.groupedPhotos = this.groupPhotosByCamera(response.photos);
-          },
-          error: () => {
-            // Optional: Handle additional error logic here
+    this._roversService.getImagesByDate(roverName, date)
+      .pipe(
+        catchError(error => {
+          this.errorMessage = 'Failed to fetch Rover images. Please try again later.';
+          console.error('Error fetching Rover metadata:', error);
+          return throwError(() => error); // Updated to use the new signature
+        })
+      )
+      .subscribe({
+        next: (response: PhotoRoot) => {
+          console.log('Rover metadata:', response);
+          if (response.photos.length === 0) {
+            this.picturesErrorMessage = 'No images found for the selected date.';
+            return;
           }
-        });
-    }
+          this.groupedPhotos = this.groupPhotosByCamera(response.photos);
+        },
+        error: () => {
+          // Optional: Handle additional error logic here
+        }
+      });
+  }
 
-    getRoverPhotosBySol(roverName: string, sol: number): void {
-      this._roversService.getImagesBySol(roverName, sol)
-        .pipe(
-          catchError(error => {
-            this.errorMessage = 'Failed to fetch Rover images. Please try again later.';
-            console.error('Error fetching Rover metadata:', error);
-            return throwError(() => error); // Updated to use the new signature
-          })
-        )
-        .subscribe({
-          next: (response: PhotoRoot) => {
-            console.log(response);
-            if (response.photos.length === 0) {
-              this.picturesErrorMessage = 'No images found for the selected date.';
-              return;
-            }
-            this.groupedPhotos = this.groupPhotosByCamera(response.photos);
-          },
-          error: () => {
-            // Optional: Handle additional error logic here
+  getRoverPhotosBySol(roverName: string, sol: number): void {
+    this._roversService.getImagesBySol(roverName, sol)
+      .pipe(
+        catchError(error => {
+          this.errorMessage = 'Failed to fetch Rover images. Please try again later.';
+          console.error('Error fetching Rover metadata:', error);
+          return throwError(() => error); // Updated to use the new signature
+        })
+      )
+      .subscribe({
+        next: (response: PhotoRoot) => {
+          console.log(response);
+          if (response.photos.length === 0) {
+            this.picturesErrorMessage = 'No images found for the selected date.';
+            return;
           }
-        });
-    }
+          this.groupedPhotos = this.groupPhotosByCamera(response.photos);
+        },
+        error: () => {
+          // Optional: Handle additional error logic here
+        }
+      });
+  }
 
-    getRoverManifest(roverName: string): void {
-      this._roversService.getMissionManifest(roverName)
-        .pipe(
-          catchError(error => {
-            this.errorMessage = 'Failed to fetch Rover images. Please try again later.';
-            console.error('Error fetching Rover metadata:', error);
-            return throwError(() => error); // Updated to use the new signature
-          })
-        )
-        .subscribe({
-          next: (response: RoverRoot) => {
-            if (response === null) {
-              this.errorMessage = 'No information about selected rover.';
-              return;
-            }
-            this.currentManifest = response.photo_manifest;
-            console.log('Rover metadata:', this.currentManifest);
-          },
-          error: () => {
-            // Optional: Handle additional error logic here
+  getRoverManifest(roverName: string): void {
+    this._roversService.getMissionManifest(roverName)
+      .pipe(
+        catchError(error => {
+          this.errorMessage = 'Failed to fetch Rover images. Please try again later.';
+          console.error('Error fetching Rover metadata:', error);
+          return throwError(() => error); // Updated to use the new signature
+        })
+      )
+      .subscribe({
+        next: (response: RoverRoot) => {
+          if (response === null) {
+            this.errorMessage = 'No information about selected rover.';
+            return;
           }
-        });
-    }
+          this.currentManifest = response.photo_manifest;
+          console.log('Rover metadata:', this.currentManifest);
+        },
+        error: () => {
+          // Optional: Handle additional error logic here
+        }
+      });
+  }
 }
