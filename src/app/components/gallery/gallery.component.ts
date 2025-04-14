8ca75f7edef349a9c5c1abfac5d0e8e8ad42cfa8
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-14 11:03:43 +0100

    Refactor gallery component: improve error handling and streamline image loading logic

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -38,53 +38,51 @@ export class GalleryComponent {
   // Handles the image loading
   getImages() {
     this._gallery.getImages()
-      .pipe(
-        catchError((error) => {
+      .subscribe({
+        next: (res) => {
+          this.images = res;
+        },
+        error: (error) => {
           this.errorMessage = 'Failed to retrieve Gallery. Please try again later.';
           console.error('Error fetching Gallery:', error);
-          return throwError(error); // Rethrow the error for further handling if needed
-        })
-      )
-      .subscribe(images => {
-        this.images = images;
-        console.log(this.images);
+        }
       });
   }
 
   deleteImage(image: GalleryImage) {
-    this._gallery.delImage(image._id)
-      .pipe(
-        catchError((error) => {
-          this.toastMessage = 'Image Deleted Succesfully!'
-          this.showToast('danger');
-          console.error('Error deleting image:', error);
-          return throwError(error); // Rethrow the error for further handling if needed
-        })
-      )
-      .subscribe(result => {
-        this.toastMessage = 'Image Deleted Succesfully!'
-        this.showToast('success');
-        console.log('Image deleted successfully:', result);
-        this.getImages(); // Refresh the image list after deletion
-      });
-  }
+        this._gallery.delImage(image._id)
+          .pipe(
+            catchError((error) => {
+              this.toastMessage = 'Image Deleted Succesfully!'
+              this.showToast('danger');
+              console.error('Error deleting image:', error);
+              return throwError(error); // Rethrow the error for further handling if needed
+            })
+          )
+          .subscribe(result => {
+            this.toastMessage = 'Image Deleted Succesfully!'
+            this.showToast('success');
+            console.log('Image deleted successfully:', result);
+            this.getImages(); // Refresh the image list after deletion
+          });
+      }
 
   openModal(image: GalleryImage) {
-    this.selectedImage = image;
-    const modalElement = document.getElementById('imageModal');
-    if (modalElement) {
-      const modal = new bootstrap.Modal(modalElement);
-      modal.show();
-    }
-  }
+        this.selectedImage = image;
+        const modalElement = document.getElementById('imageModal');
+        if(modalElement) {
+          const modal = new bootstrap.Modal(modalElement);
+          modal.show();
+        }
+      }
 
   showToast(type: string) {
-    const toastEl = document.getElementById('liveToast');
-    if (toastEl) {
-      this.toastInstance = new bootstrap.Toast(toastEl);
-      this.toastType = type;
-      this.toastInstance?.show();
-    }
-  }
+        const toastEl = document.getElementById('liveToast');
+        if(toastEl) {
+          this.toastInstance = new bootstrap.Toast(toastEl);
+          this.toastType = type;
+          this.toastInstance?.show();
+        }
+      }
 
 }
