438d292e0a41b6d24000284460903872deb33a29
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-14 11:03:06 +0100

    Implement modal for asteroid details and enhance layout for better user interaction

--- src/app/components/asteroids/asteroids.component.ts
+++ src/app/components/asteroids/asteroids.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { catchError } from 'rxjs/operators';
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AsteroidsService } from '../../services/asteroids.service';
 import { Asteroids, Asteroid, NearEarthObjects } from '../../models/asteroids';
-import { SimpleCacheService } from '../../services/simple-cache.service';
+import { AsteroidDetailsComponent } from "../asteroid-details/asteroid-details.component";
+
+declare var bootstrap: any; // declare bootstrap for TS
 
 @Component({
   selector: 'app-asteroids',
-  imports: [RouterModule, CommonModule, FormsModule],
+  imports: [RouterModule, CommonModule, FormsModule, AsteroidDetailsComponent],
   templateUrl: './asteroids.component.html',
   styleUrl: './asteroids.component.css'
 })
@@ -20,23 +22,30 @@ export class AsteroidsComponent implements OnInit {
   asteroids: Asteroid[] = [];
   neoData: Asteroid[] = [];
   startDate: string = '';
-  endDate: string= '';
+  endDate: string = '';
   errorMessage: string = '';
+  selectedAsteroid: Asteroid | undefined;
 
-  constructor(private _asteroidsService: AsteroidsService, private simpleCache: SimpleCacheService) { }
+  @Output() onModalOpen: EventEmitter<string>;
+
+  constructor(private _asteroidsService: AsteroidsService) {
+    this.onModalOpen = new EventEmitter();
+  }
 
   ngOnInit(): void {
+    this.asteroids = []; // Initialize to an empty array
     const today = new Date();
     this.endDate = today.toISOString().split('T')[0]; // Format as YYYY-MM-DD
     this.startDate = today.toISOString().split('T')[0]; // Format as YYYY-MM-DD
-    
-    // Set the current items from the items in the cache
-    if (this.simpleCache.items.length && this.areItemsAsteroids(this.simpleCache.items)) {
-      this.asteroids = this.simpleCache.items;
-    }
-    else {
-      this.asteroids = []; // Initialize to an empty array if the cache is empty or invalid
-      this.simpleCache.items = []; // Clear the cache if it's invalid
+  }
+
+  ngAfterViewInit() {
+    const modalElement = document.getElementById('imageModal');
+    if (modalElement) {
+      modalElement.addEventListener('hidden.bs.modal', () => {
+        this.selectedAsteroid = undefined;
+        console.log('Modal closed, asteroid cleared');
+      });
     }
   }
 
@@ -45,25 +54,19 @@ export class AsteroidsComponent implements OnInit {
     window.location.reload();
   }
 
-  // TypeGuard
-  private isAsteroid(item: any): item is Asteroid {
-    return item && 
-    typeof item.name === 'string' && 
-    typeof item.nasa_jpl_url === 'string' &&
-    typeof item.id === 'string' &&
-    typeof item.is_potentially_hazardous_asteroid === 'boolean'
-  } 
-
-  // TypeGuard for an array of Asteroid objects
-  private areItemsAsteroids(items: any[]): items is Asteroid[] {
-    return items.every(item => this.isAsteroid(item));
+  openModal(asteroid: Asteroid) {
+    this.selectedAsteroid = asteroid;
+    this.onModalOpen.emit(this.selectedAsteroid.id);
+    const modalElement = document.getElementById('imageModal');
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
   }
 
   // Function to fetch Asteroids
   loadData() {
     this.asteroids = []; // Clear the pictures array when the button is clicked
-    this.simpleCache.items = []; // Clear the cache when the button is clicked
-    this.simpleCache.sanitizedURLs = {}; // Clear the sanitized URLs cache
     this.getAsteroids(this.startDate, this.endDate); // Fetch data
   }
 
@@ -74,7 +77,7 @@ export class AsteroidsComponent implements OnInit {
   }
 
   // Calling AsteroidsService to get the asteroids
-  getAsteroids(startDate:string, endDate:string) {
+  getAsteroids(startDate: string, endDate: string) {
     this._asteroidsService.getAsteroids(startDate, endDate)
       .pipe(
         catchError(error => {
@@ -84,7 +87,6 @@ export class AsteroidsComponent implements OnInit {
         })
       ).subscribe((response: Asteroids) => {
         this.asteroids = this.extractAsteroids(response.near_earth_objects);
-        this.simpleCache.items = this.asteroids; // Cache the data
         console.log(this.asteroids);
       });
   }
