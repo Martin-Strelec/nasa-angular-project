b6230f5ea871dec26d69bd41eed9739c13bfb355
Martin-Strelec <m.strelec2021@gmail.com>
2025-04-21 17:38:13 +0100

    Remove ApodDetails route: clean up unused routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { AsteroidsComponent } from './components/asteroids/asteroids.component';
 import { RoversComponent } from './components/rovers/rovers.component';
 import { EpicComponent } from './components/epic/epic.component';
 import { AsteroidDetailsComponent } from './components/asteroid-details/asteroid-details.component';
-import { ApodDetailsComponent } from './components/apod-details/apod-details.component';
 import { HomeComponent } from './components/home/home.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 
@@ -15,6 +14,5 @@ export const routes: Routes = [
  {path: "rovers", component: RoversComponent},
  {path: "epic", component: EpicComponent},
  {path: "asteroid-details/:id", component: AsteroidDetailsComponent},
- {path: "apod-details/:date", component: ApodDetailsComponent},
  {path: "gallery", component: GalleryComponent}
 ];
